9c7caddde9de75f31c8c1f11a531a103ce7edac2
Preston Mills <Plutoxian@users.noreply.github.com>
2017-03-08 22:12:21 -0600

    base bingo things

---
+++ script/test-bingo.js
@@ -0,0 +1,140 @@
+var bingoList = [];
+bingoList[1] = [
+  { name: "\"?\" in all chat", types: ["powerbomb"] },
+  { name: "\"ez\" in all chat", types: ["spazer"] },
+  { name: "Killsteal", types: ["sporespawn"] },
+  { name: "someone gets a rampage", types: ["sporespawn"] }
+];
+bingoList[2] = [
+  { name: "\"Well Played!\"", types: ["powerbomb"] },
+  { name: "teammate does his best", types: ["supermissile"] },
+  { name: "teamwork making the dream work", types: ["energytank", "missile"] },
+  { name: "space creation", types: ["crateriamap"] }
+];
+bingoList[3] = [
+  { name: "100+ strength", types: ["powerbomb"] },
+  { name: "100+ agility", types: ["icebeam"] },
+  { name: "100+ intelligence", types: ["energytank"] },
+  { name: "argument over mid", types: ["missile"] },
+  { name: "immediately regretted tp", types: ["chargebeam"] }
+];
+bingoList[4] = [
+  { name: "support dreams of buying expensive items", types: ["hijump"] },
+  { name: "unnecessarily clutch save", types: ["missile"] },
+  { name: "giving up prematurely", types: ["missilerecharge", "brinstar"] },
+  { name: "offensive name", types: ["icebeam", "hijump"] }
+];
+bingoList[5] = [
+  { name: "someone is shocked by an item purchase already announced by a teammate", types: ["energytank", "powerbomb"] },
+  { name: "permabash", types: ["missile", "areamissile"] },
+  { name: "single camp stacked 3 times", types: ["statueroom"] },  // go into the statues room and witness the destruction of Kraid/Phantoon/Draygon/Ridley
+  { name: "cliffing someone", types: ["spazer"] }
+];
+bingoList[6] = [
+  { name: "teammate autoattacking lane creeps", types: ["energytank"] },
+  { name: "random noise coming from teammate's mic", types: ["reservetank"] },
+  { name: "rngesus smiles upon someone", types: ["icebeam"] },
+  { name: "excessive swearing", types: ["powerbomb"] },
+  { name: "WAY too many auras on one hero (enough to make you say “aw heck”)", types: ["crocomire"] }
+];
+bingoList[7] = [
+  { name: "\"no support\" in all chat", types: ["norfairmap"] },
+  { name: "multi-carry lane", types: ["wavebeam"] },
+  { name: "ranged oov", types: ["crocomire", "hijump"] },
+  { name: "big ult completely missed (bh, echo, rp, etc.)", types: ["missile"] },
+  { name: "3 raze combo", types: ["energytank"] }
+];
+bingoList[8] = [
+  { name: "lotus orb reflects a minor inconvenience", types: ["wavebeam"] },
+  { name: "clarity/salve canceled within 2 seconds", types: ["energytank", "missile"] },
+  { name: "no supports on a team", types: ["grapplebeam", "crocomire"] }
+];
+bingoList[9] = [
+  { name: "racial slurs in all chat", types: ["maridiamap"] },
+  { name: "<4 bloodstone charges", types: ["powerbomb"] },
+  { name: "cm/wd ults for longer than necessary", types: ["missile"] },
+  { name: "carry flames a competent support", types: ["missile"] }
+];
+bingoList[10] = [
+  { name: "muted teammate", types: ["wavebeam", "speedbooster"] },
+  { name: "bkb does nothing but make someone shiny", types: ["speedbooster", "grapplebeam"] },
+  { name: "teammate afk for crucial teamfight", types: ["icebeam", "speedbooster"] }
+];
+bingoList[11] = [
+  { name: "everyone is level 25", types: ["missile"] },
+  { name: "last-second tp", types: ["supermissile"] },
+  { name: "\"can someone buy detection\"-into-not-buying-detection combo", types: ["powerbomb"] },
+  { name: "walking cour 10+ min in", types: ["energytank"] }
+];
+bingoList[12] = [
+  { name: "5 man shrine usage", types: ["supermissile"] },
+  { name: "an excessive amounts of tangoes", types: ["powerbomb"] },
+  { name: ">20 bloodstone charges", types: ["reservetank"] }
+];
+bingoList[13] = [
+  { name: "ping spam", types: ["supermissile"] },
+  { name: "invoker forgets he has 10 spells", types: ["energytank", "wsetroom"] },
+  { name: "mid doesn't buy items to rush bottle", types: ["reservetank"] }
+];
+bingoList[14] = [
+  { name: "forgetting to glyph a t1 tower", types: ["finish", "forgottenarea"] },
+  { name: "THAT'S A HOOK (either clutch or buggy hooks)", types: ["finish"] }
+];
+bingoList[15] = [
+  { name: "carry misses first cs", types: ["finish"] },
+  { name: "slark dies in shadow dance", types: ["finish"] },
+  { name: "4 wards in stock", types: ["finish", "wsetroom"] }
+];
+bingoList[16] = [
+  { name: "5 or more illusions of the same hero", types: ["supermissile"] },
+  { name: "game-losing random pick", types: ["wsmap"] },
+  { name: "5 man slardar ravage", types: ["energytank", "xrayscope"] }
+];
+bingoList[17] = [
+  { name: "non-meta boot choice", types: ["supermissile"] },
+  { name: "ratting attempt doesn’t work at all", types: ["energytank"] }
+];
+bingoList[18] = [
+  { name: "divine rapier purchase", types: ["supermissile"] },
+  { name: "10 min queue", types: ["speedbooster"] },
+  { name: "taunt-into-get-bodied combo", types: ["gravitysuit", "variasuit"] }
+];
+bingoList[19] = [
+  { name: "passive hotd", types: ["variasuit", "gravitysuit"] },
+  { name: "rubick steals amazing spell and chokes immediately", types: ["forgottenarea"] },
+  { name: "zeus ult multikill", types: ["supermissile", "powerbomb"] }
+];
+bingoList[20] = [
+  { name: "causing a tree massacre", types: ["crateriamap", "wsmap"] },
+  { name: "mk needs no introduction (you’ll know when to check this)", types: ["reservetank"] },
+  { name: "jungle lc", types: ["missile", "supermissile"] }
+];
+bingoList[21] = [
+  { name: "sniper uses shrapnel on tower", types: ["botwoon"] },
+  { name: "sniper tries to stack shrapnel", types: ["powerbomb"] }
+];
+bingoList[22] = [
+  { name: "storm spirit does that tp thing (must hit someone)", types: ["gt", "sporespawn"] },
+  { name: "butterfly flutter actually does something", types: ["ridley"] },
+  { name: "clueless riki walks under sentry ward (MUST be clueless)", types: ["draygon"] }
+];
+bingoList[23] = [
+  { name: "pa dagger obliterates someone", types: ["screwattack"] },
+  { name: "more lightning procs than edison", types: ["plasma", "ridley"] },
+  { name: "manta dodge", types: ["spacejump", "draygon"] },
+  { name: "someone ignores blade mail", types: ["chargebeam", "spazer", "wavebeam", "icebeam", "plasmabeam"] },
+];
+bingoList[24] = [
+  { name: "zoning eclipse (2 or fewer hits)", types: ["plasmabeam", "draygon"] },
+  { name: "BLACK HOOOOLE (4+ players)", types: ["crocomire", "ridley"] },
+  { name: "axe both spins and wins", types: ["plasmabeam", "spacejump"] },
+  { name: "timelapse-into-get-obliterated combo", types: ["spacejump", "hijump"] }
+];
+bingoList[25] = [
+  { name: "caribou (note: must take picture of caribou for certification)", types: ["screwattack", "botwoon"] },
+  { name: "omnislash does a heck ton damage", types: ["supermissile"] },
+  { name: "mana void multikill", types: ["energytank"] },
+  { name: "someone actually buys a daedalus", types: ["plasmabeam", "screwattack"] }
+];
+
+$(function() { srl.bingo(bingoList, 5); });
